187cb6ad11f27e13a5764426ec3202cbec832a5c
goodjwon <firstman9@protonmail.com>
2021-05-07 17:11:46 +0900

    파일 저장 설정영역 추가.

--- myApiFiles/src/main/java/kr/my/files/property/FileStorageProperties.java
+++ myApiFiles/src/main/java/kr/my/files/property/FileStorageProperties.java
@@ -2,11 +2,14 @@ package kr.my.files.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
     private String uploadDir;
+    private String publicSpaceDir;
+    private String protectSpaceDir;
 
     public String getUploadDir() {
         return uploadDir;
@@ -14,5 +17,21 @@ public class FileStorageProperties {
 
     public void setUploadDir(String uploadDir) {
         this.uploadDir = uploadDir;
+    }
+
+    public String getPublicSpaceDir() {
+        return publicSpaceDir;
+    }
+
+    public void setPublicSpaceDir(String publicSpaceDir) {
+        this.publicSpaceDir = publicSpaceDir;
+    }
+
+    public String getProtectSpaceDir() {
+        return protectSpaceDir;
+    }
+
+    public void setProtectSpaceDir(String protectSpaceDir) {
+        this.protectSpaceDir = protectSpaceDir;
     }
 }
