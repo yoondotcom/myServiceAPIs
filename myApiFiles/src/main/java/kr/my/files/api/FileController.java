45986e036d6864d905e7b3ab2278b7ad815df613
kimjiyun <didany15@dongguk.ac.kr>
2021-07-19 00:32:50 +0900

    테스트 ui 작업중

--- myApiFiles/src/main/java/kr/my/files/api/FileController.java
+++ myApiFiles/src/main/java/kr/my/files/api/FileController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.core.io.Resource;
